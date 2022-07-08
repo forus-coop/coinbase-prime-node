6551f42785a66a8f6a7666bf440cef7f233a3fb2
Triveni Badgujar <triveni.badgujar@synerzip.com>
2022-07-08 12:53:35 +0530

    Balances and products APIs

---
+++ src/lib/balances.ts
@@ -0,0 +1,37 @@
+// Base class to connect to coinbase
+import { formatResponse, ResponseFormat } from "./util";
+import { Client } from './client';
+
+type portfolioParams = { balance_type?: string, symbol?: string }
+export class Balances extends Client {
+
+  // Get balance for a specific wallet
+  // ==== Path Params
+  // * +portfolio_id+   - [String] ID of portfolio
+  // * +wallet_id+      - [String] ID of wallet
+  // ==== Returns
+  // @return [Hash] a hash with list of balances
+  async wallet<ResponseFormat>(wallet_id: string) {
+    const operationalUrl = `${this.portfolioUri()}/wallets/${wallet_id}/balance`
+    const res = await this.get(operationalUrl);
+    return formatResponse(res);
+  }
+
+  // List all balances for a specific portfolio
+  // ==== Path Params
+  // * +portfolio_id+  - [String] ID of portfolio
+  // ==== params
+  // * +balance_type+  - [String] A type by which to filter balances
+  //                     TRADING_BALANCES: Trading balances
+  //                     VAULT_BALANCES: Vault balances
+  //                     TOTAL_BALANCES: Total balances
+  // * +symbols+       - [String] A list of symbols by which to filter
+  //                     the response
+  // ==== Returns
+  // @return [Hash] a hash with balances
+  async portfolio<ResponseFormat>(params: portfolioParams = {}) {
+    const operationalUrl = `${this.portfolioUri()}/balances`
+    const res = await this.get(operationalUrl, params);
+    return formatResponse(res);
+  }
+}
