08981b02d16f542e9d61ec4ab057f99ad3b5d79f
Prasann Sawant <prasann.sawant@forusall.com>
2022-07-08 13:59:42 +0530

    added activities api

---
+++ src/lib/activities.ts
@@ -0,0 +1,39 @@
+import { Client } from "./client";
+import { formatResponse, ResponseFormat } from "./util";
+
+export class Activities extends Client {
+
+  // Gets a list of address book Activities.
+  // ==== options
+  //
+  // * +currency_symbol+ - [String]  A Currency symbol of address (optional)
+  // * +symbols+ - [Array] Filter by list of currencies (optional)
+  // * +categories+ - [Array] Filter by list of activity categories [order,allocation] (optional)
+  // * +statuses+ - [Array] Filter by list of statuses(optional)
+  // * +start_time+ - [String] Filter created time by start date (optional)
+  // * +end_time+ - [String] Filter created time by end date (optional)
+  // * +cursor+ - [String] Id to retrieve additional results strictly after (optional)
+  // * +limit+ - [Int] Filter created time by end date (optional)
+  // * +sort_direction+ - [String] Sorting order (optional)
+  // ==== Returns
+  // * [Hash] a hash with list of Activities
+  async list<ResponseFormat>(params = {}) {
+    return formatResponse(
+      await this.get(`${this.portfolioUri()}/activities`, params)
+    );
+  }
+
+
+  // Get Order by Activity Id
+  // ==== options
+  //
+  // * +activityId+ -  [String] the valid activity id.
+  //
+  // ==== Returns
+  // * [Hash] a hash with status code and activity
+  async fetch<ResponseFormat>(activityId:string ,params = {}) {
+    return formatResponse(
+      await this.get(`${this.portfolioUri()}/activities/${activityId}`, params)
+    );
+  }
+}
