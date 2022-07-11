bef1b37f85075abb4f85e6f2aa3c3574588f3c95
Triveni Badgujar <triveni.badgujar@synerzip.com>
2022-07-11 12:16:20 +0530

    Added tests for client lib

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
 };
-
+process.env = Object.assign(process.env, {
+  PORTFOLIO_ID: 'test_portfolio',
+  ENTITY_ID: 'test_entity'
+});
